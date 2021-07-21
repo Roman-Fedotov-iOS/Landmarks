72e1c155d01b219d12ad7b2787f73368fedc7c2e
Roman-Fedotov-iOS <romafed23@gmail.com>
2021-07-21 20:13:48 +0300

    2

---
+++ Landmarks/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Roman Fedotov on 26.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
